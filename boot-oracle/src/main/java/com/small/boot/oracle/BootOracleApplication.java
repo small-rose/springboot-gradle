18fe965b1b7fa9ed29abad7a40d7f9541fda7c47
small-rose <small-rose@qq.com>
2023-01-22 12:11:53 +0800

    add thread demo

---
+++ boot-oracle/src/main/java/com/small/boot/oracle/BootOracleApplication.java
@@ -0,0 +1,13 @@
+package com.small.boot.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootOracleApplication.class, args);
+    }
+
+}
---
+++ boot-oracle/src/test/java/com/small/boot/oracle/BootOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.small.boot.oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootOracleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
