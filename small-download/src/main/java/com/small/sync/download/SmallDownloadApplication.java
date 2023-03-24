813ecf14543301c781b014f9fdc8c1e49743fa94
small-rose <small-rose@qq.com>
2023-03-24 23:26:46 +0800

    :sparkles: 异步导出

---
+++ small-download/src/main/java/com/small/sync/download/SmallDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.small.sync.download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallDownloadApplication.class, args);
+    }
+
+}
---
+++ small-download/src/test/java/com/small/sync/download/SmallDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.small.sync.download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
